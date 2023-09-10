0d5743723d03db6eb143c81e24557a2ba63b58ae
Vijitha Epa <vijithaepa@gmail.com>
2023-09-10 14:11:07 +1000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <UserHome />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Need to refresh too.
         </p>
         <a
           className="App-link"
