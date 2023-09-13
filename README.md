9cd21d64280938c6c1bbf3805e11799caa360f21
Vijitha Epa <vijithaepa@gmail.com>
2023-09-13 22:52:40 +1000

    integration 2

--- README.md
+++ README.md
@@ -102,6 +102,7 @@ sudo apt update
 sudo apt install git
 sudo apt install docker.io
 sudo apt install docker-compose
+sudo chmod 666 /var/run/docker.sock
 
 ### nginx
 /etc/nginx/sites-available/default
--- src/services/ContainerService.ts
+++ src/services/ContainerService.ts
@@ -4,18 +4,17 @@
 
     export const getUserGreeting = () => {
         console.log('Service got called');
-        return fetch('http://0.0.0.0:8080/docker/greeting', {
+        return fetch('http://localhost:8080/docker/greeting', {
             method: 'GET',
             // headers: {
             // }
         }).then(res => {
-            // console.log('response ', res.body, res.formData())
+            console.log('response ', res.body, res.formData())
             return res.text()
         })
-        //     .then(res => {
-        //     console.log('Next response ', res)
-        //     return res;
-        // })
+        .catch(error => {
+            console.log('Error response ', error)
+        })
     }
 // }
 
