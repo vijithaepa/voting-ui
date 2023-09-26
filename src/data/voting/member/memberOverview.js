27f3d92e6a549a13395fa424b17d8ace2ac718a7
Vijitha Epa <vijithaepa@gmail.com>
2023-09-26 12:59:03 +1000

    overview

---
+++ src/data/voting/member/memberOverview.js
@@ -0,0 +1,27 @@
+import student from "../../../assets/img/e-learning/avatar/student.png";
+
+export const memberOverview = {
+  info: {
+    title: 'member of parliament',
+    name: 'Michael Jackson',
+    lastUpdate: '03/11/2021',
+    age: 75,
+    email: 'abc@gmamil.com',
+    phoneNo: '+231394820',
+    image: student
+  },
+  voting: {
+    positive: {
+      votes: [85, 60, 120, 70, 100, 15, 65, 80, 60, 75, 45],
+      total: 1043,
+      rate: 6.4,
+      diff: -5.3
+    },
+    negative: {
+      votes: [55, 60, 40, 120, 70, 80, 35, 80, 85],
+      total: 723,
+      rate: 8.1,
+      diff: -3.5
+    }
+  }
+}
